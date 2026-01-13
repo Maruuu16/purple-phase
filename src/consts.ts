c190070bf4d89a89d6074fed1d5341ff4465f3b8
Maruuu16 <chiroze0120@gmail.com>
2026-01-14 01:58:08 +0900

    タイトル変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'maru.log';
+export const SITE_DESCRIPTION = 'ベルギー滞在妻の海外生活ブログ。猫不足な日常をゆるく綴っています。';
